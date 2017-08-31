aea869fb9b7496cab71f9bfe4bc504e6e5ae5960
Anthony <anthonyu234@gmail.com>
2017-08-31 15:31:23 +0100

    fixed pending transactions

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -32,11 +32,11 @@ class UserDashboardController extends Controller
 
     public function index(){
 
-        $btc_wallet_id = CreateAddress::where('user_id', Auth::User()->id)->first();
-        $btc_wallet = $btc_wallet_id->btc_wallet_id;
+       // $btc_wallet_id = CreateAddress::where('user_id', Auth::User()->id)->first();
+       // $btc_wallet = $btc_wallet_id->btc_wallet_id;
 
-        $btc_balance   = new BlockIoTest();
-        $balance  = $btc_balance->getbalance($btc_wallet);
+        //$btc_balance   = new BlockIoTest();
+        //$balance  = $btc_balance->getbalance($btc_wallet);
 
         $current_price = new BlockIoTest();
         $current_price_usd = $current_price->CurrentPriceInUsd();
@@ -44,7 +44,7 @@ class UserDashboardController extends Controller
         $ExchangeRate = new ExchangeRate();
         $presentRateNaira   = $ExchangeRate->rate();
 
-       // $cancel = CancledMail::where('user_id',Auth::User()->id)->get();
+       $cancel = CancledMail::where('user_id',Auth::User()->id)->get();
         $market = MarketPlace::where('user_id',Auth::User()->id)->get();
 
         $amount_balance = DB::table('authorizations')
@@ -62,7 +62,7 @@ class UserDashboardController extends Controller
 
         $total_balance = $amount_balance_total - $withdrawal_total;
 
-         return view('dashboard.home',compact('btc_wallet_id','balance','current_price_usd','presentRateNaira','market', 'total_balance'));
+         return view('dashboard.home',compact('current_price_usd','presentRateNaira','cancel','market', 'total_balance'));
         // return view('dashboard.home');
     }
 
