704127576140603b3482f7a3ac62c67ec4524d65
Anthony <anthonyu234@gmail.com>
2017-09-01 03:15:15 +0100

    test

--- resources/views/dashboard/layout/header.blade.php
+++ resources/views/dashboard/layout/header.blade.php
@@ -13,7 +13,7 @@
               <!-- Navbar Header-->
               <div class="navbar-header">
                 <!-- Navbar Brand --><a href="/" class="navbar-brand">
-                  <div class="brand-text brand-big hidden-lg-down"><span>Pay </span><strong>Miso</strong></div>
+                  <div class="brand-text brand-big hidden-lg-down"><span>Pay </span><strong>Misoo</strong></div>
                   <div class="brand-text brand-small"><strong>ME</strong></div></a>
                 <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
               </div>
