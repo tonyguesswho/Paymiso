f5114728334a8d4676f8b3243172cbdbdc69e29c
Anthony <anthonyu234@gmail.com>
2017-09-04 04:23:27 +0100

    recaptcha

--- resources/views/email/transactionEmail.blade.php
+++ resources/views/email/transactionEmail.blade.php
@@ -1,175 +1,109 @@
-<!DOCTYPE html>
-<html lang="">
+  <!DOCTYPE html>
+  <html>
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Verify Email</title>
+    <title>paymiso</title>
+  <style type="text/css">
+      .wrapper{width:60%; margin:5% auto;  box-shadow:0 0 2px #aaa; font-family:Hind;}
+      .logo_header{width:100%; height:70px;background:#8DC53F;}
+      .email_body{width:100%; padding:0 10px;}
+      .receipt_list{width:100%;}
+      .receipt_list .left_list{float:left; width:35%;}
+      .receipt_list .right_list{float:left; width:65%;}
+      .left_list b,.right_list b{width:100%; float:left; margin:0 0 10px 0;}
+      .left_list span,.right_list span{width:100%; float:left; margin:0 0 5px 0;}
+      .right_list span{text-align:left; padding-left: 2px;}
+      .list_divider{width:100%; border-top:1px solid rgba(0,0,0,0.2);float:left;}
+      
+      .invoice_left{float:left; width:40%;}
+      .invoice_right{float:left; width:60%;}
+      h2{
+        display:inline-block;
+      }
+      .btn{
+          margin: 4px;
+          
+          width: 6em;
+          height: 50px;
+          font-size: 18px;
+         }
+        .btn-sunny {
+          color: #fff;
+          background-color:#f5b75f ;
+          border-bottom:2px solid #c38a3a;
+          }
 
-    <!-- Bootstrap CSS -->
-   
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-   <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-   
-    <style type="text/css">
-      body {
-  background-color: #CCCCCC;
-}
-
-body,p, h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4 {
-  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-  font-weight: 300;
-  line-height: 2em;
-
-}
-p{
-  font-size: 17px;
-}
-a, a:hover, a:focus {
-  color: #9c27b0;
-}
-.nav-tabs {
-  background: #9c27b0;
-  border: 0;
-  border-radius: 3px;
-  padding: 0 15px;
-}
-.nav-tabs > li > h4 {
-  color: #FFFFFF;
-  border: 0;
-  margin: 0;
-  border-radius: 3px;
-  line-height: 34px;
-  text-transform: uppercase;
-  font-size: 15px;
-  
-}
-
-.card {
-  display: inline-block;
-  position: relative;
-  width: 100%;
-  border-radius: 3px;
-  color: rgba(0,0,0, 0.87);
-  background: #fff;
-  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-}
-
-.card .card-content {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-}
-
-.card .content {
-  padding: 15px;
-}
-
-.card .header {
-  box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56), 0 4px 25px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
-  margin: 15px;
-  border-radius: 3px;
-  padding: 15px 0;
-  background-color: #FFFFFF;
-}
-
-.card .header-success {
-  background: linear-gradient(60deg, #66bb6a, #388e3c);
-}
-
-.card-nav-tabs {
-  margin-top: 60px;
-}
-.card-nav-tabs .header {
-  margin-top: -30px;
-}
-.card-nav-tabs .nav-tabs {
-  background: transparent;
-}
-.btn {
-  border: none;
-  border-radius: 3px;
-  position: relative;
-  padding: 12px 30px;
-  margin: 10px 1px;
-  font-size: 14px;
-  font-weight: 400;
-  text-transform: lowercase;
-  letter-spacing: 0;
-  will-change: box-shadow, transform;
-  transition: box-shadow 0.2s cubic-bezier(0.4, 0, 1, 1), background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-
-.btn.btn-primary, .btn.btn-primary:hover, .btn.btn-primary:focus, .btn.btn-primary:active, .btn.btn-primary.active, .btn.btn-primary:active:focus, .btn.btn-primary:active:hover, .btn.btn-primary.active:focus, .btn.btn-primary.active:hover{
-  background-color: #03a9f4;
-  color: #FFFFFF;
-}
-.btn.btn-primary:focus, .btn.btn-primary:active, .btn.btn-primary:hover {
-  
-  box-shadow: 0 14px 26px -12px rgba(3, 169, 244, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(3, 169, 244, 0.2);
-}
-
-</style>
-
-    
+       .btn-fresh {
+          color: #fff;
+          background-color: #8DC53F;
+          border-bottom:2px solid #41996c;
+          }
+    </style>
+     <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
   </head>
   <body>
     <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-            <div class="title text-center">
-              <h3>paymiso</h3>
-            </div>
-
-            <!-- Tabs with icons on Card -->
-            <div class="card card-nav-tabs">
-              <div class="header header-success">
-                <!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
-                <div class="nav-tabs-navigation">
-                  <div class="nav-tabs-wrapper">
-                    <ul class="nav nav-tabs" data-tabs="tabs">
-                      <li class="active" style="width: 100%;">
-                        <h4 class="text-center">
-                          
-                          Please verify your account to enjoy all our services
-                        </h4>
-                      </li>
-                      
-                    </ul>
-                  </div>
+  <div class="row">
+            
+            <div class="wrapper">
+                <div class="logo_header">
+                    <a href=""><img src=""/></a>
                 </div>
-              </div>
-              <div class="content">
-                <div class="tab-content text-center">
-                  <div class="tab-pane active" id="profile">
-                 <p> An Email has been sent to you</p>
-                 <p>Click on the resend button if you did not receive the email</p>
-                  <button class="btn btn-primary">RESEND</button>
+                <div class="email_banner">
                     
-                  </div>
-                  
                 </div>
-              </div>
-            </div>
-            <span style="font-family: Arial, Helvetica, sans-serif; font-size: 13px; color: #96a5b5;">
-          2017 © PayMiso Technologies LTD. ALL Rights Reserved.
-        </span>
-            <!-- End Tabs with icons on Card -->
-
-          </div>
-          </div>
-          </div>
-
-    
-
-    <!-- jQuery -->
-     <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
-    <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-    
+                <div class="email_body">
+                    <h1 class="text-center">Bitcoin Transaction Details</h1>
+                    <p>
+                        Kindly go through the details below before making payment
+                    </p>
+                    
+                    <div class="receipt_list">
+                        <div class="left_list">
+                          
+                            <span>Seller's Name:</span>
+                            <span>Buyer's Id:</span>
+                            <span>Amount BTC:</span>
+                            <span>Amount USD:</span>
+                            <span>Rate:</span>
+                            <span>Amount NGN:</span>
+                            <span>Escrow fee (0.75% for each participant):</span>
+                            
+                        </div>
+                          <div class="right_list">
+                            
+                                 <span>{{$sellcoin->User->firstname}}&nbsp{{$sellcoin->User->lastname}}</span>
+                                 <span>{{$sellcoin->wallet_id}}</span>
+                                 <span>{{$sellcoin->amount_btc}}</span>
+                                 <span>{{number_format($sellcoin->amount_dollar,2)}}</span>
+                                 <span>{{$sellcoin->rate}}</span>
+                                 <span>{{number_format($sellcoin->amount_dollar*$sellcoin->rate,2)}}</span>
+                                 <span>{{number_format(($sellcoin->amount_dollar*$sellcoin->rate)*0.0075,2)}}</span>
+                        </div>
+                        <span class="list_divider"></span>
+                         <div class="left_list">
+                             <b>Total</b>
+                              </div>
+                          <div class="right_list">
+                               <span><b>{{$sellcoin->amount_dollar*$sellcoin->rate + (($sellcoin->amount_dollar*$sellcoin->rate)*0.0075)}}</b></span>
+                          </div>
+                    </div>
+                    <div class="row">
+                     <div class="invoice_trans col-lg-6">
+                        <div class="invoice_left">
+                           <a href="{{route('payEmailDone',['id' => $sellcoin->Transaction->sell_coin_id,'token' => $sellcoin->Transaction->transaction_token])}}"><button type="button" class="btn btn-fresh text-uppercase">Confirm</button></a>
+                        </div>
+                    </div>
+                     <div class="invoice_trans col-lg-6">
+                        <div class="invoice_right">
+                           <a href="{{route('cancelEmailDone',['id'=>$sellcoin->Transaction->sell_coin_id,'token'=>$sellcoin->Transaction->transaction_token])}}"><button type="button" class="btn btn-sunny text-uppercase">Cancel</button></a>
+                        </div>
+                    </div> 
+                  </div>
+  </div>
+</div>
+  
   </body>
-</html>
+  </html>
+ 
+  
+        
