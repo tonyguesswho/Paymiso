7880aaf3183f4819d9acad4e57aa4967e38f57f1
tony <anthonyu234@gmail.com>
2017-11-04 03:06:22 +0100

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,10 @@
                         <li><a href="https://blog.paymiso.com/" target="_blank">Blog</a></li>
                         <li><a href="/marketPlace" target="_blank">Marketplace</a></li>
                     </ul>
-                    <h1 id="fh5co-logo"><a href="{{ url('/') }}"><img src="images/logown.jpg" alt="Image" class="img-responsive"><span></span></a></h1>
+                    <ul>
+                    <h1 id="fh5co-logo"><a href="{{ url('/') }}"><img src="images/logown.jpg" alt="Image"
+                     class="img-responsive"><span></span></a></h1>
+                    </ul>
                     <ul class="pull-right right-menu">
                         @if(Auth::guest())
                         
