c13808ba9bf3aed4b930bcfe1217398067177b6e
tony <anthonyu234@gmail.com>
2017-11-04 03:18:31 +0100

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
                         <li><a href="/marketPlace" target="_blank">Marketplace</a></li>
                     </ul>
                     <ul>
-                    <h1 id="fh5co-logo"><a href="{{ url('/') }}"><img src="images/logow.jpg" alt="Image"
+                    <h1 id="fh5co-logo"><a href="{{ url('/') }}"><img src="images/logown.jpg" alt="Image"
                      class="img-responsive"><span></span></a></h1>
                     </ul>
                     <ul class="pull-right right-menu">
