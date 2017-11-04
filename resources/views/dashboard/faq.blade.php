27c8e8ae9c7e1f85cb6e60c05c41876f457d5b19
tony <anthonyu234@gmail.com>
2017-11-04 17:50:17 +0100

    faq

--- resources/views/dashboard/faq.blade.php
+++ resources/views/dashboard/faq.blade.php
@@ -6,7 +6,7 @@
         <div class="panel panel-default ">
             <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question0">
                  <h4 class="panel-title">
-                    <a href="#" class="ing">Q: How does Paymiso work</a>
+                    <a href="#" class="ing">Q: How does Paymiso work?</a>
               </h4>
 
             </div>
@@ -19,7 +19,7 @@
                         <li>Seller creates and funds his/her Paymiso bitcoin wallet</li>
                         <li>Seller starts the trasaction and the details are sent to the buyer</li>
                         <li>Buyer confirms the details and makes payment via our payment gateway</li>
-                        <li>Buyer instantly gets butcoin funds in his/her wallet</li>
+                        <li>Buyer instantly gets bitcoin funds in his/her wallet</li>
                         <li>Seller can withdraw from his/her paymiso dashboard instantly</li>
                     </ul>
 
@@ -29,7 +29,7 @@
         <div class="panel panel-default ">
             <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question1">
                  <h4 class="panel-title">
-                    <a href="#" class="ing">Q: Why do we use it?</a>
+                    <a href="#" class="ing">Q: Is there any trasactional cost?</a>
               </h4>
 
             </div>
@@ -37,14 +37,14 @@
                 <div class="panel-body">
                      <h5><span class="label label-primary">Answer</span></h5>
 
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <p>Paymiso charges 0.75% escrow fee from the transacting parties</p>
                 </div>
             </div>
         </div>
         <div class="panel panel-default ">
             <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question2">
                  <h4 class="panel-title">
-                    <a href="#" class="ing">Q: Where does it come from?</a>
+                    <a href="#" class="ing">Q: What is network fee?</a>
               </h4>
 
             </div>
@@ -52,14 +52,15 @@
                 <div class="panel-body">
                      <h5><span class="label label-primary">Answer</span></h5>
 
-                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                    <p>The network fee is a very small fee charged by the bitcoin network for the 
+                      miners in other to enable smooth transfer of bitcoin between wallets</p>
                 </div>
             </div>
         </div>
         <div class="panel panel-default ">
             <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
                  <h4 class="panel-title">
-                    <a href="#" class="ing">Q: Where can I get some?</a>
+                    <a href="#" class="ing">Q: Who bears the cost of the network fee?</a>
               </h4>
 
             </div>
@@ -67,8 +68,96 @@
                 <div class="panel-body">
                      <h5><span class="label label-primary">Answer</span></h5>
 
-                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+                    <p>The network is fee is deducted from the wallet of the bitcoin seller i.e
+                     the seller is responsible for providing enough funds
+                      needed to complete the trasaction inclusive of network fee  </p>
                 </div>
+
+            </div>
+        </div>
+        <div class="panel panel-default ">
+            <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+                 <h4 class="panel-title">
+                    <a href="#" class="ing">Q: How can i know the network fee?</a>
+              </h4>
+
+            </div>
+            <div id="question3" class="panel-collapse collapse" style="height: 0px;">
+                <div class="panel-body">
+                     <h5><span class="label label-primary">Answer</span></h5>
+
+                    <p>Paymiso provides an estimated network fee, which in most cases will be higher than the actual network fee that you will be eventually charged.This is 
+                    to account for fluctuations in network fees  </p>
+                </div>
+
+            </div>
+        </div>
+        <div class="panel panel-default ">
+            <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+                 <h4 class="panel-title">
+                    <a href="#" class="ing">Q: Am i paying the actual network fee or the estimated network fee?</a>
+              </h4>
+
+            </div>
+            <div id="question3" class="panel-collapse collapse" style="height: 0px;">
+                <div class="panel-body">
+                     <h5><span class="label label-primary">Answer</span></h5>
+
+                    <p>The network fee is not a paymiso charge so you get to pay the actual network fee.Paymiso 
+                      provides the estimated network fee just to make sure there is enough funds in the seller 
+                      account even if there is fluctuation in the network fee after some hours  </p>
+                </div>
+
+            </div>
+        </div>
+        <div class="panel panel-default ">
+            <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+                 <h4 class="panel-title">
+                    <a href="#" class="ing">Q: How long does a trasaction last?</a>
+              </h4>
+
+            </div>
+            <div id="question3" class="panel-collapse collapse" style="height: 0px;">
+                <div class="panel-body">
+                     <h5><span class="label label-primary">Answer</span></h5>
+
+                    <p>An escrow transaction is open for one(1) hour i.e the buyer is 
+                      expected to pay in an hour or the transaction will be closed   </p>
+                </div>
+
+            </div>
+        </div>
+        <div class="panel panel-default ">
+            <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+                 <h4 class="panel-title">
+                    <a href="#" class="ing">Q: As a seller how can i get my money?</a>
+              </h4>
+
+            </div>
+            <div id="question3" class="panel-collapse collapse" style="height: 0px;">
+                <div class="panel-body">
+                     <h5><span class="label label-primary">Answer</span></h5>
+
+                    <p>Sellers can withdraw their cash from their dashboard.
+                      The amount will be trasanfered to any Nigerian bank account </p>
+                </div>
+
+            </div>
+        </div>
+        <div class="panel panel-default ">
+            <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+                 <h4 class="panel-title">
+                    <a href="#" class="ing">Q: How can i get more information?</a>
+              </h4>
+
+            </div>
+            <div id="question3" class="panel-collapse collapse" style="height: 0px;">
+                <div class="panel-body">
+                     <h5><span class="label label-primary">Answer</span></h5>
+
+                    <p>Reach out to us via support@paymiso.com with further enquiry. </p>
+                </div>
+
             </div>
         </div>
         
