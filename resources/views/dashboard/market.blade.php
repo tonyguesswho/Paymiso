b490f770c7f9e4ec9c5838383fa4ba9cb857a766
tony <anthonyu234@gmail.com>
2017-11-04 03:42:32 +0100

    logo

--- resources/views/dashboard/market.blade.php
+++ resources/views/dashboard/market.blade.php
@@ -32,7 +32,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/home">Paymiso</a>
+          <a class="navbar-brand" href="/home"><img src="images/logownn.png"></a>
         </div>
     
         <!-- Collect the nav links, forms, and other content for toggling -->
